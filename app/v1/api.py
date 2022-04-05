081dd125a09b35be873b3f595ec194a1e55e43a7
setiad1 <dev.setiadi.acc@gmail.com>
2022-04-06 04:17:43 +0700

    Add file for adiministrator router aka endpoints

--- app/v1/api.py
+++ app/v1/api.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
-from app.v1.routers import user, settings, subscription, client, playground, home
+from app.v1.routers import user, settings, subscription, client, playground, home, administrator
 
 
 router_v1 = APIRouter()
 router_v1.include_router(home.router, prefix="", tags=["home"])
 router_v1.include_router(user.router, prefix="/v1/user", tags=["user"])
-router_v1.include_router(settings.router, prefix="/v1/settings", tags=["settings"])
-router_v1.include_router(subscription.router, prefix="/v1/subscription", tags=["subscription"])
+router_v1.include_router(administrator.router, prefix="/v1/administrator", tags=["administrator"])
 router_v1.include_router(client.router, prefix="/v1/client", tags=["client"])
+router_v1.include_router(subscription.router, prefix="/v1/subscription", tags=["subscription"])
+router_v1.include_router(settings.router, prefix="/v1/settings", tags=["settings"])
 router_v1.include_router(playground.router, prefix="/v1/playground", tags=["playground"])
---
+++ app/v1/routers/administrator.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from app.core.database import db_session
+from app.core.utils.auth import get_current_active_user
+from app.core.models.user import User
+
+router = APIRouter()
+
+
+@router.get("/")
+async def hello(current_user: User = Depends(get_current_active_user)):
+    return {"data": current_user}
