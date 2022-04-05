8c6fe6eace3198490b12377de8e337d6e050b752
setiad1 <dev.setiadi.acc@gmail.com>
2022-04-06 04:29:55 +0700

    Move list user and update group endpoints to administrator router

--- app/v1/routers/administrator.py
+++ app/v1/routers/administrator.py
@@ -2,11 +2,47 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from app.core.database import db_session
 from app.core.utils.auth import get_current_active_user
+from app.core.utils import  auth
 from app.core.models.user import User
+from app.core.schemas import user as schema_user
+from app.v1.services import user as service_user
+
 
 router = APIRouter()
 
 
-@router.get("/")
-async def hello(current_user: User = Depends(get_current_active_user)):
-    return {"data": current_user}
+@router.get("/list-user/", response_model=list[schema_user.UserDetailOut], status_code=status.HTTP_200_OK)
+async def list_user(db: Session = Depends(db_session)):
+    user_list = service_user.user_list(db=db)
+    try:
+        if not user_list:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="List data pengguna tidak ditemukan",
+            )
+        return user_list
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Terjadi kesalahan dari sistem"
+        )
+    finally:
+        db.close()
+
+
+@router.put("/update-group/", response_model=schema_user.UserDetailOut, dependencies=[Depends(auth.default)], status_code=status.HTTP_200_OK)
+async def update_group(schema: schema_user.GroupUpdate, current_user: User = Depends(auth.get_current_active_user), db: Session = Depends(db_session)):
+    try:
+        current_user.group_id = schema.group_id
+        update_data = schema.dict(exclude_unset=True)
+        for key, value in update_data.items():
+            setattr(current_user, key, value)
+        db.add(current_user)
+        db.commit()
+
+        dt_user = service_user.find_user_by_username_3(username=current_user.username, db=db)
+        return dt_user
+    except Exception:
+        raise
+    finally:
+        db.close()
--- app/v1/routers/user.py
+++ app/v1/routers/user.py
@@ -121,24 +121,6 @@ async def update_profile(
         db.close()
 
 
-@router.put("/update-group/", response_model=schema_user.UserDetailOut, dependencies=[Depends(auth.default)], status_code=status.HTTP_200_OK)
-async def update_group(schema: schema_user.GroupUpdate, current_user: User = Depends(auth.get_current_active_user), db: Session = Depends(db_session)):
-    try:
-        current_user.group_id = schema.group_id
-        update_data = schema.dict(exclude_unset=True)
-        for key, value in update_data.items():
-            setattr(current_user, key, value)
-        db.add(current_user)
-        db.commit()
-
-        dt_user = service_user.find_user_by_username_3(username=current_user.username, db=db)
-        return dt_user
-    except Exception:
-        raise
-    finally:
-        db.close()
-
-
 @router.patch("/update-password/", status_code=status.HTTP_201_CREATED)
 async def update_password(
         schema: schema_user.UpdatePassword,
@@ -227,22 +209,6 @@ async def activation(schema: schema_user.Activation, db: Session = Depends(db_se
     else:
         response = "Kode aktivasi sudah tidak berlaku"
     return {"data": response}
-
-
-@router.get("/list-user/", response_model=list[schema_user.UserDetailOut], status_code=status.HTTP_200_OK)
-async def list_user(db: Session = Depends(db_session)):
-    user_list = service_user.user_list(db=db)
-    try:
-        if not user_list:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="List data pengguna tidak ditemukan",
-            )
-        return user_list
-    except Exception:
-        raise
-    finally:
-        db.close()
 
 
 @router.post("/user-detail/", response_model=schema_user.UserDetailOut, dependencies=[Depends(auth.default)], status_code=status.HTTP_200_OK)
